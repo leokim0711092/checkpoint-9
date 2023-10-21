45b7cdeb1188614b853bf62f02baa48275abe7b7
leokim0711092 <leokim10073@gmail.com>
2023-10-21 11:18:43 +0000

    Get argument and launch finish

--- src/pre_approach.cpp
+++ src/pre_approach.cpp
@@ -1,11 +1,13 @@
 #include "geometry_msgs/msg/detail/twist__struct.hpp"
 #include "nav_msgs/msg/detail/odometry__struct.hpp"
 #include "rclcpp/executors.hpp"
+#include "rclcpp/logging.hpp"
 #include "rclcpp/node.hpp"
 #include "rclcpp/publisher.hpp"
 #include "rclcpp/subscription.hpp"
 #include "rclcpp/utilities.hpp"
 #include "sensor_msgs/msg/detail/laser_scan__struct.hpp"
+#include <cmath>
 #include <functional>
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
@@ -18,8 +20,12 @@ class Attach_self : public rclcpp::Node{
 
     public:
         Attach_self(int &argc, char **argv): Node("attach_self"){
-            obstacle = std::stof(argv[2]);
-            degree = std::stoi(argv[4]);
+            
+
+            this->declare_parameter("obstacle", 0.0);
+            this->declare_parameter<int>("degrees", 0);
+
+            getting_params();
 
             sub_scan = this->create_subscription<sensor_msgs::msg::LaserScan>("/scan", 10 , 
             std::bind(&Attach_self::scan_callback, this, std::placeholders::_1));
@@ -33,21 +39,47 @@ class Attach_self : public rclcpp::Node{
 
     private:
         float obstacle;
-        int degree;
+        int degrees;
         geometry_msgs::msg::Twist vel;
         rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr sub_scan;
         rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_odom;
+        bool turn;
 
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
 
         void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
-
-        
+            RCLCPP_INFO(this->get_logger(), "obstacle: %f", obstacle);
+            RCLCPP_INFO(this->get_logger(), "degrees: %i", degrees );
+            if(msg->ranges[359] > obstacle){
+                vel.linear.x = 0.2;
+            }else if (msg->ranges[359] < obstacle){
+                vel.linear.x = 0;
+                turn = true;
+            }
         }
 
         void odom_callback(const nav_msgs::msg::Odometry msg){
-            
-        
+            float cur_degree = euler_degree_transform(msg)/M_PI*360;
+            RCLCPP_INFO(this->get_logger(), "cur_degrees: %f", cur_degree );
+            if (degrees - cur_degree > 1 && turn ) {
+                vel.angular.z = (degrees - cur_degree)*M_PI/360*0.5 ;
+            }else{
+                vel.angular.z = 0;
+            }
+        }
+
+        void getting_params(){
+            obstacle = this->get_parameter("obstacle").get_parameter_value().get<float>();
+            degrees = this->get_parameter("degrees").as_int();
+        }
+
+        float euler_degree_transform(const nav_msgs::msg::Odometry t_st){
+                float x = t_st.pose.pose.orientation.x;
+                float y = t_st.pose.pose.orientation.y;
+                float z = t_st.pose.pose.orientation.z;
+                float w = t_st.pose.pose.orientation.w; 
+
+            return atan2(2 * (w * z + x * y),1 - 2 * (y * y + z * z));
         }
 
 };
